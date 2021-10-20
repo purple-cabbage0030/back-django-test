c4bed40622fa844b5cc64eb01d6aa870e32f8b78
Hongju Lee <litehj84@gmail.com>
2021-10-20 18:08:56 +0900

    register 로직, activity 데이터 insert

--- member/views.py
+++ member/views.py
@@ -11,7 +11,7 @@ from .models import Users
 from .serializers import UserSerializer
 
 # 회원가입
-@api_view(['POST'])
+@api_view(['GET','POST'])
 def registrationView(request):
     if request.method == 'POST':
         print('111111111111')
@@ -21,14 +21,14 @@ def registrationView(request):
         if serializer.is_valid():
             print('got valid')
             new_user = Users(
-                uuid = serializer.validated_data['UUID'], 
-                upw = serializer.validated_data['UPW'],
-                uage = int(serializer.validated_data['UAGE']), 
-                usex = serializer.validated_data['USEX'],
-                uheight = float(serializer.validated_data['UHEIGHT']),
-                uweight = float(serializer.validated_data['UWEIGHT']),
-                uact = serializer.validated_data['UACT'], 
-                urdc = float(serializer.validated_data['URDC'])
+                uuid = serializer.validated_data['uuid'], 
+                upw = serializer.validated_data['upw'],
+                uage = int(serializer.validated_data['uage']), 
+                usex = serializer.validated_data['usex'],
+                uheight = float(serializer.validated_data['uheight']),
+                uweight = float(serializer.validated_data['uweight']),
+                uact = serializer.validated_data['uact'], 
+                urdc = float(serializer.validated_data['urdc'])
                 )
             new_user.save()
             data['response'] = "회원가입 성공"
@@ -38,3 +38,5 @@ def registrationView(request):
         print(data)
         return JsonResponse(data)
 
+    else:
+        print('데이터 안 옴')
