3a5df7b233c36bc74a358faa56c68da1f08af6b5
Hongju Lee <litehj84@gmail.com>
2021-10-20 10:24:46 +0900

    model update

--- member/models.py
+++ member/models.py
@@ -1,15 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 class Activity(models.Model):
     uact = models.CharField(primary_key=True, max_length=30)
     val = models.FloatField()
 
 class Users(models.Model):
-    uuid = models.EmailField(primary_key=True)
-    upw = models.CharField(max_length=15)
-    uage = models.IntegerField()
-    usex = models.CharField(max_length=7)
-    uheight = models.FloatField()
-    uweight = models.FloatField()
-    uact = models.ForeignKey(Activity, on_delete=models.PROTECT)
-    urdc = models.FloatField()
+    uuid = models.EmailField(primary_key=True, verbose_name='이메일')
+    upw = models.CharField(max_length=15, verbose_name='비밀번호')
+    uage = models.IntegerField(verbose_name='나이')
+    SEX_CHOICE = [
+        ['F', '여성'],
+        ['M', '남성 ']
+    ]
+    usex = models.CharField(max_length=1, choices=SEX_CHOICE, verbose_name='성별')
+    uheight = models.FloatField(verbose_name='키')
+    uweight = models.FloatField(verbose_name='몸무게')
+    uact = models.ForeignKey(Activity, on_delete=models.PROTECT, verbose_name='활동량')
+    urdc = models.FloatField(verbose_name='권장칼로리')
---
+++ member/serializers.py
@@ -0,0 +1,9 @@
+# from rest_framework import serializers
+# from rest_framework.validators import UniqueTogetherValidator
+
+# from models import Users
+
+# class UserSerializer(serializers.ModelSerializer):
+
+#     def create(self, validated_data):
+#         user = Users.objects.create
