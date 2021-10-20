4d0d1c1e0d8c358fd5f959ced7840a5a2ae11469
Henry-choi426 <84162474+Henry-choi426@users.noreply.github.com>
2021-10-20 16:32:07 +0900

    다이어트세이브업데이트

--- repository/urls.py
+++ repository/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
-from .views import helloAPI, predictFood
+from .views import helloAPI, predictFood, dietSave
 
 app_name = 'repository'
 urlpatterns = [
-    path('hello/', helloAPI, name='hello'),
-    path('predict/', predictFood, name='prediction')
+    path('hello', helloAPI, name='hello'),
+    path('predict', predictFood, name='prediction'),
+    path('dietsave', dietSave, name='dietsave')
 ]
