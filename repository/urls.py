ea74be8fc7d490d8cbcbfda04f03ec355e9b2b3e
Hongju Lee <litehj84@gmail.com>
2021-10-20 18:33:45 +0900

    merge

--- repository/urls.py
+++ repository/urls.py
@@ -4,10 +4,6 @@ from .views import helloAPI, predictFoodView, dietSaveView
 app_name = 'repository'
 urlpatterns = [
     path('hello', helloAPI, name='hello'),
-<<<<<<< HEAD
     path('predict', predictFoodView, name='prediction'),
     path('dietsave', dietSaveView, name='dietsave')
-=======
-    path('predict', predictFood, name='prediction')
->>>>>>> e793930 (registration update)
 ]
