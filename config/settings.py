a332df6a2977064d5151dc2948ee63e7a2fc044e
Hongju Lee <litehj84@gmail.com>
2021-11-06 11:41:58 +0900

    Nginx 적용

--- config/settings.py
+++ config/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-import os, json
+import os
 from pathlib import Path
 from django.core.exceptions import ImproperlyConfigured
 
